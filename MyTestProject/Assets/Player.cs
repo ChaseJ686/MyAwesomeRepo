6256a3b17c967d4e77e2ff13db9e8cb65b9cacca
ChaseJ686 <JACOBSC3@my.erau.edu>
2025-01-17 14:45:14 -0700

    Proper Commands

--- MyTestProject/Assets/Player.cs
+++ MyTestProject/Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,12 +8,31 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.5f;
+        }
+
+        if (Input.HetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.25f;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.25f;
+        }
     }
 }
